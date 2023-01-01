b2dadc20cb017c600d5934d8257cb9a9bebb0804
Daniel de Jesus <danieldejesus8745@gmail.com>
2023-01-01 18:34:04 -0300

    chore: initial commit

---
+++ src/main/java/com/quanto/QuantoApplication.java
@@ -0,0 +1,13 @@
+package com.quanto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantoApplication.class, args);
+	}
+
+}
